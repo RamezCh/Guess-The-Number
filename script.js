e01572539922cd08046cf5a3b9f4ca234bc8af07
RamezCh <ramiz_chreide@outlook.com>
2024-07-05 16:27:43 +0300

    Style Edits, JS

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const message = document.querySelector('header h1');
+console.log(message.textContent);
+const rangeNumber = document.querySelector('.number');
+console.log(rangeNumber.textContent);
