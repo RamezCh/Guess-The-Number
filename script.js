a1f987eefd4c24d65f0b65e6d472793aef335072
RamezCh <ramiz_chreide@outlook.com>
2024-07-06 13:28:44 +0300

    JS querySelector

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 'use strict';
 
 const message = document.querySelector('header h1');
-console.log(message.textContent);
 const rangeNumber = document.querySelector('.number');
+const score = document.querySelector('.score');
+const guess = document.querySelector('.guess');
+
+console.log(message.textContent);
 console.log(rangeNumber.textContent);
+console.log(score.textContent);
+console.log(guess.value);
+
+const checkButton = document.querySelector('.check');
+checkButton.addEventListener('click', function () {
+  console.log('Button clicked');
+  console.log(guess.value);
+});
