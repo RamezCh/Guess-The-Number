6664ef9ac7c4d5199cdb081a5036dcf5bc850a31
RamezCh <ramiz_chreide@outlook.com>
2024-07-06 18:33:21 +0300

    Fix Bug

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ let score = 5;
 // Generate Random Number between 1 and 20
 // Math.random() generates number between 0 and 1
 // Math.trunc() removes decimal part
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 const checkButton = document.querySelector('.check');
 const againButton = document.querySelector('.again');
+const guessElement = document.querySelector('.guess');
 // Event Listener for Check Button
 checkButton.addEventListener('click', function () {
   // Value is String, Convert to Number
-  const guessElement = document.querySelector('.guess');
   const guess = Number(guessElement.value);
   console.log(guess, typeof guess);
   // If No Number, we get 0 which is Falsy value
@@ -27,8 +27,10 @@ checkButton.addEventListener('click', function () {
     rangeNumber.textContent = secretNumber;
     // Adds inline style to body
     document.querySelector('body').style.backgroundColor = '#60b347';
-    // Adds inline style to number
-    rangeNumber.style.width = '30rem';
+    // Hide Guess Input and Check Button - Show Again Button
+    guessElement.style.display = 'none';
+    checkButton.style.display = 'none';
+    againButton.style.visibility = 'visible';
   } else if (guess > secretNumber * 2 && score > 1) {
     // When Guess is too high
     message.textContent = 'Too High!';
@@ -53,6 +55,7 @@ checkButton.addEventListener('click', function () {
     // When Player Loses
     message.textContent = 'You Lost the Game!';
     scoreElement.textContent = 0;
+    // Hide Guess Input and Check Button - Show Again Button
     guessElement.style.display = 'none';
     checkButton.style.display = 'none';
     againButton.style.visibility = 'visible';
@@ -68,7 +71,6 @@ againButton.addEventListener('click', function () {
   guessElement.value = '';
   // Reset Styles
   document.querySelector('body').style.backgroundColor = '#222';
-  rangeNumber.style.width = '15rem';
   // Generate Random Number between 1 and 20
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   // Show Guess Input and Check Button - Hide Again Button
